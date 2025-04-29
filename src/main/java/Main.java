8a7be68b5ddefc5278d30e9bc4d2dac2459fd06e
bluhhar <bluhhar@gmail.com>
2025-04-29 21:09:10 +0300

    backpropagande error init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
         //test1();
-        test2();
+        //test2();
+        test3();
     }
 
     private static void test1() {
@@ -40,5 +41,40 @@ public class Main {
 
         double[] prediction = network.predict(new double[]{0.5, 0.2, 0.7, 0.1});
         System.out.println(Arrays.toString(prediction));
+    }
+
+    private static void test3() {
+        String resourcePath = "datasets/iris.csv";
+        URL resourceUrl = Main.class.getClassLoader().getResource(resourcePath);
+        Path path = null;
+        try {
+            path = Paths.get(resourceUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        List<Iris> dataset = IrisDataReader.load(path.toString());
+
+        NeuralNetwork network = new NeuralNetwork();
+        network.addLayer(8, new Sigmoid());
+        network.addLayer(3, new Sigmoid());
+
+        double learningRate = 0.1;
+        int epochs = 1000;
+
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            double totalError = 0.0;
+            for (Iris sample : dataset) {
+                network.train(sample.features, sample.label, learningRate);
+
+                // Вычисляем ошибку (сумма квадратов ошибок)
+                double[] output = network.predict(sample.features);
+                for (int i = 0; i < output.length; i++) {
+                    totalError += Math.pow(sample.label[i] - output[i], 2);
+                }
+            }
+            if (epoch % 100 == 0) {
+                System.out.println("Эпоха " + epoch + ", Ошибка: " + totalError);
+            }
+        }
     }
 }
--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -29,4 +29,56 @@ public class NeuralNetwork {
         }
         return output;
     }
+
+    public void train(double[] input, double[] target, double learningRate) {
+        // 1. Прямой проход
+        predict(input);
+
+        // 2. Вычисление ошибки на выходном слое
+        Layer outputLayer = layers.getLast();
+        for (int i = 0; i < outputLayer.getNeurons().length; i++) {
+            Neuron neuron = outputLayer.getNeurons()[i];
+            double error = target[i] - neuron.getOutput();
+            double delta = error * neuron.getActivationFunction().derivative(neuron.getInputSum());
+            neuron.setDelta(delta);
+        }
+
+        // 3. Вычисление ошибки для скрытых слоёв
+        for (int l = layers.size() - 2; l >= 0; l--) { // от предпоследнего к первому
+            Layer currentLayer = layers.get(l);
+            Layer nextLayer = layers.get(l + 1);
+
+            for (int i = 0; i < currentLayer.getNeurons().length; i++) {
+                Neuron neuron = currentLayer.getNeurons()[i];
+                double sum = 0.0;
+                for (Neuron nextNeuron : nextLayer.getNeurons()) {
+                    sum += nextNeuron.getWeights()[i] * nextNeuron.getDelta();
+                }
+                double delta = sum * neuron.getActivationFunction().derivative(neuron.getInputSum());
+                neuron.setDelta(delta);
+            }
+        }
+
+        // 4. Обновление весов и смещений
+        double[] prevOutputs = input; // на входе первый слой получает input
+        for (Layer layer : layers) {
+            double[] newPrevOutputs = new double[layer.getNeurons().length];
+            for (int i = 0; i < layer.getNeurons().length; i++) {
+                Neuron neuron = layer.getNeurons()[i];
+                double delta = neuron.getDelta();
+
+                // Обновляем веса
+                for (int j = 0; j < neuron.getWeights().length; j++) {
+                    double oldWeight = neuron.getWeights()[j];
+                    neuron.setWeight(j, oldWeight + learningRate * delta * prevOutputs[j]);
+                }
+
+                // Обновляем смещение
+                neuron.setBias(neuron.getBias() + learningRate * delta);
+
+                newPrevOutputs[i] = neuron.getOutput();
+            }
+            prevOutputs = newPrevOutputs;
+        }
+    }
 }
